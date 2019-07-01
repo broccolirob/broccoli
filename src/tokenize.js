a27833791944416f1240c3c950d22e10e3b314bc
broccolirob <rschn4911@gmail.com>
2019-07-01 12:40:26 -0700

    Add tokens for number, name, and parens.

--- src/tokenize.js
+++ src/tokenize.js
@@ -11,7 +11,41 @@ const tokenize = input => {
   let cursor = 0;
 
   while (cursor < input.length) {
-    cursor++;
+    const char = input[cursor];
+
+    if (isParenthesis(char)) {
+      tokens.push({
+        type: "Parenthesis",
+        value: char
+      });
+      cursor++;
+      continue;
+    }
+
+    if (isWhitespace(char)) {
+      cursor++;
+      continue;
+    }
+
+    if (isNumber(char)) {
+      tokens.push({
+        type: "Number",
+        value: parseInt(char, 10)
+      });
+      cursor++;
+      continue;
+    }
+
+    if (isLetter(char)) {
+      tokens.push({
+        type: "Name",
+        value: char
+      });
+      cursor++;
+      continue;
+    }
+
+    throw new Error(`${char} is not a valid character`);
   }
 
   return tokens;
--- src/tokenize.test.js
+++ src/tokenize.test.js
@@ -4,4 +4,52 @@ describe("tokenize", () => {
   it("should return an array", () => {
     expect(Array.isArray(tokenize(""))).toBe(true);
   });
+
+  it("should be able to tokenize a pair of parentheses", () => {
+    const input = "()";
+    const result = [
+      { type: "Parenthesis", value: "(" },
+      { type: "Parenthesis", value: ")" }
+    ];
+
+    expect(tokenize(input)).toEqual(result);
+  });
+
+  it("should ignore whitespace", () => {
+    const input = "       ";
+    expect(tokenize(input)).toEqual([]);
+  });
+
+  it("should correctly tokenize a single digit", () => {
+    const input = "2";
+    const result = [{ type: "Number", value: 2 }];
+
+    expect(tokenize(input)).toEqual(result);
+  });
+
+  it("should be able to handle single numbers in expressions", () => {
+    const input = "(1 2)";
+
+    const result = [
+      { type: "Parenthesis", value: "(" },
+      { type: "Number", value: 1 },
+      { type: "Number", value: 2 },
+      { type: "Parenthesis", value: ")" }
+    ];
+
+    expect(tokenize(input)).toEqual(result);
+  });
+
+  it("should be able to handle single letters in expressions", () => {
+    const input = "(a b)";
+
+    const result = [
+      { type: "Parenthesis", value: "(" },
+      { type: "Name", value: "a" },
+      { type: "Name", value: "b" },
+      { type: "Parenthesis", value: ")" }
+    ];
+
+    expect(tokenize(input)).toEqual(result);
+  });
 });
