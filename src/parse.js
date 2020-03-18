f549759f0d1146f9a85aa4d3d04ede8c07bb1c27
broccolirob <rschn4911@gmail.com>
2020-03-18 01:08:56 -0400

    Basic AST return.

--- src/parse.js
+++ src/parse.js
@@ -1,10 +1,33 @@
 const { isOpeningParenthesis, isClosingParenthesis } = require("./helpers");
 const { pop, peek } = require("./utilities");
 
-const parenthesize = tokens => tokens;
+const parenthesize = tokens => {
+  const token = pop(tokens);
+
+  if (isOpeningParenthesis(token.value)) {
+    const expression = [];
+
+    while (!isClosingParenthesis(peek(tokens).value)) {
+      expression.push(parenthesize(tokens));
+    }
+
+    pop(tokens);
+    return expression;
+  }
+  return token;
+};
 
 const parse = tokens => {
-  const token = pop(tokens);
+  if (Array.isArray(tokens)) {
+    const [first, ...rest] = tokens;
+    return {
+      type: "CallExpression",
+      name: first.value,
+      arguments: rest.map(parse)
+    };
+  }
+
+  const token = tokens;
 
   if (token.type === "Number") {
     return {
--- tests/parse.test.js
+++ tests/parse.test.js
@@ -21,4 +21,59 @@ describe(parse, () => {
 
     expect(parse(tokens)).toEqual(ast);
   });
+
+  it("should return an AST for a basic data structure", () => {
+    const tokens = [
+      { type: "Parenthesis", value: "(" },
+      { type: "Name", value: "add" },
+      { type: "Number", value: 2 },
+      { type: "Number", value: 3 },
+      { type: "Parenthesis", value: ")" }
+    ];
+
+    const ast = {
+      type: "CallExpression",
+      name: "add",
+      arguments: [
+        { type: "NumericLiteral", value: 2 },
+        { type: "NumericLiteral", value: 3 }
+      ]
+    };
+
+    expect(parse(tokens)).toEqual(ast);
+  });
+
+  it("should return an AST for a nested data structure", () => {
+    const tokens = [
+      { type: "Parenthesis", value: "(" },
+      { type: "Name", value: "add" },
+      { type: "Number", value: 2 },
+      { type: "Number", value: 3 },
+      { type: "Parenthesis", value: "(" },
+      { type: "Name", value: "subtract" },
+      { type: "Number", value: 4 },
+      { type: "Number", value: 2 },
+      { type: "Parenthesis", value: ")" },
+      { type: "Parenthesis", value: ")" }
+    ];
+
+    const ast = {
+      type: "CallExpression",
+      name: "add",
+      arguments: [
+        { type: "NumericLiteral", value: 2 },
+        { type: "NumericLiteral", value: 3 },
+        {
+          type: "CallExpression",
+          name: "subtract",
+          arguments: [
+            { type: "NumericLiteral", value: 4 },
+            { type: "NumericLiteral", value: 2 }
+          ]
+        }
+      ]
+    };
+
+    expect(parse(tokens)).toEqual(ast);
+  });
 });
